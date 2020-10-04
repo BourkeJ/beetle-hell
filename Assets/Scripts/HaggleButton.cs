f5acc397836bc19082349c2f13a8a37b4e876a27
Jen <bourkj@rpi.edu>
2020-10-04 16:37:32 -0400

    Add dialogue to timeshare scene

--- Assets/Scripts/HaggleButton.cs
+++ Assets/Scripts/HaggleButton.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class HaggleButton : MonoBehaviour
 {
     [SerializeField] private TimeshareBeetle _timeshareBeetle = null;
+    [SerializeField] private string[] _beetleDialogue = null;
+    [SerializeField] private Text _beetleDialogueText = null;
 
     public void OnClick()
     {
         _timeshareBeetle._hagglePressed = true;
+        _beetleDialogueText.text =_beetleDialogue[Random.Range(0, _beetleDialogue.Length)];
     }
-
 }
