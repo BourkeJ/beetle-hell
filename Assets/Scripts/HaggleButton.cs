97b0864a08217bbd7e8bb87ee6309e899844c642
Jen <bourkj@rpi.edu>
2020-10-04 13:33:37 -0400

    Remove bar from timeshare scene

--- Assets/Scripts/HaggleButton.cs
+++ Assets/Scripts/HaggleButton.cs
@@ -6,26 +6,6 @@ using UnityEngine.UI;
 public class HaggleButton : MonoBehaviour
 {
     [SerializeField] private TimeshareBeetle _timeshareBeetle = null;
-    [SerializeField] private RectTransform _barHash = null;
-    [SerializeField] private float _clickLeftPos = 0f;
-    [SerializeField] private float _clickRightPos = 0f;
-
-    private Button _haggleButton = null;
-
-    void Awake()
-    {
-        _haggleButton = GetComponent<Button>();
-    }
-
-    void Update()
-    {
-        if (_barHash.anchoredPosition.x >= _clickLeftPos && _barHash.anchoredPosition.x <= _clickRightPos) 
-        {
-            _haggleButton.interactable = true;
-        } else{
-            _haggleButton.interactable = false;
-        }
-    }
 
     public void OnClick()
     {
--- Assets/Scripts/TimeshareBeetle.cs
+++ Assets/Scripts/TimeshareBeetle.cs
@@ -5,44 +5,12 @@ using UnityEngine.UI;
 
 public class TimeshareBeetle : MonoBehaviour
 {
-    private float _barPosY = 0f;
-    private bool _atLeft = false;
-    private float _movePos = 1f;
-    
-    //[SerializeField] private float _clickLeftPos = 0f;
-    //[SerializeField] private float _clickRightPos = 0f;
-    [SerializeField] private RectTransform _barHash = null;
-    [SerializeField] private float _barLeftPos = 0f;
-    [SerializeField] private float _barRightPos = 10f;
-    [SerializeField] private float _moveTime = 1f;
     [SerializeField] private Text _price = null;
 
     public bool _hagglePressed = false;
 
-    void Start()
-    {
-        _barPosY = _barHash.anchoredPosition.y;
-    }
-
     void Update()
     {
-        // bar hash osccilation
-        if(_barLeftPos <= _barHash.anchoredPosition.x + 1f && _barLeftPos >= _barHash.anchoredPosition.x - 1f)
-        {
-            _atLeft = true;
-        }
-        if(_barRightPos >= _barHash.anchoredPosition.x - 1f && _barRightPos <= _barHash.anchoredPosition.x + 1f){
-            _atLeft = false;
-        }
-
-        if(_atLeft == true){
-            _movePos += _moveTime * Time.deltaTime;
-        }else{
-            _movePos -= _moveTime * Time.deltaTime;
-        }
-        
-        _barHash.anchoredPosition = new Vector2 (Mathf.Lerp(_barLeftPos, _barRightPos, _movePos), _barPosY);
-
         //reset price when haggle button pressed
         if (_hagglePressed == true){
             float priceNum = Random.Range(1000, 10000000);
