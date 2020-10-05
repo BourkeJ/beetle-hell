d654bd2ae7e72514b8940654998145cb2dfe7c1e
pee <leonardo.price@gmail.com>
2020-10-05 16:53:12 -0400

    fish sound

---
+++ Assets/slutsound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slutsound : MonoBehaviour
+{
+    private AudioSource audSor;
+    public AudioClip reelSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audSor = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void reel()
+    {
+        
+        audSor.PlayOneShot(reelSound);
+
+    }
+}
