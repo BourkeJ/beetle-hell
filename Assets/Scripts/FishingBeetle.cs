0e78a061fe76de67a0c367c3b45496a2705b7218
Jen <bourkj@rpi.edu>
2020-10-05 16:25:46 -0400

    Finish slut ending

--- Assets/Scripts/FishingBeetle.cs
+++ Assets/Scripts/FishingBeetle.cs
@@ -12,15 +12,19 @@ public class FishingBeetle : MonoBehaviour
     [SerializeField] private float _clickLeftPos = 0f;
     [SerializeField] private float _clickRightPos = 0f;
     [SerializeField] private Animator _slutAnim = null;
+    [SerializeField] private float _buttonTime = 1f;
+    [SerializeField] private GameObject _buttonText = null;
 
     private float _barPosY = 0f;
     private bool _atLeft = false;
     private float _movePos = 1f;
+    private int _fishIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         _barPosY = _barHash.anchoredPosition.y;
+        _buttonText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -48,8 +52,19 @@ public class FishingBeetle : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && _barHash.anchoredPosition.x >= _clickLeftPos && _barHash.anchoredPosition.x <= _clickRightPos) 
         {
             _slutAnim.SetTrigger("Pressed");
+            _fishIndex += 1;
         }else if(Input.GetMouseButtonDown(0) && (_barHash.anchoredPosition.x <= _clickLeftPos || _barHash.anchoredPosition.x >= _clickRightPos)){
             _slutAnim.SetTrigger("Goofed");
+            if(_fishIndex -1 >= 0){
+                _fishIndex -= 1;
+            }
+        }
+
+        if (_fishIndex == 3){
+            _buttonTime -= Time.deltaTime;
+            if(_buttonTime <= 0f){
+                _buttonText.SetActive(true);
+            }
         }
     }
 }
