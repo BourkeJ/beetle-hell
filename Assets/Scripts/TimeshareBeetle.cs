d8d831ced0979006f8db66689b26941b7d567604
Jen <bourkj@rpi.edu>
2020-10-05 16:42:16 -0400

    Add caching to timeshare

--- Assets/Scripts/TimeshareBeetle.cs
+++ Assets/Scripts/TimeshareBeetle.cs
@@ -12,21 +12,27 @@ public class TimeshareBeetle : MonoBehaviour
     [SerializeField] private float _beetleWaitTime = 10f;
     [SerializeField] private string _sceneName = "TimeshareBeetle";
     [SerializeField] private GameObject _offerButton = null;
+    [SerializeField] private AudioSource _cachingTwo = null;
+
 
     public bool _hagglePressed = false;
     public bool _offerAccepted = false;
+
+    private AudioSource _audioSource = null;
 
     //private bool _offerButtonOn = false;
 
     void Awake()
     {
         _offerButton.SetActive(false);
+        _audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         //reset price when haggle button pressed
         if (_hagglePressed == true){
+            _audioSource.Play();
             _offerButton.SetActive(true);
             float priceNum = Random.Range(1000, 10000000);
             _price.text = priceNum.ToString();
@@ -34,6 +40,8 @@ public class TimeshareBeetle : MonoBehaviour
         }
 
         if(_offerAccepted == true){
+            //_cachingTwo.Play();
+            //_audioSource.Play();
             _beetleDialogueText.text = _beetleDialogue;
             _beetleWaitTime -= Time.deltaTime;
             if(_beetleWaitTime <= 0f){
